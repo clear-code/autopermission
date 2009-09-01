23724e1a8697e0dcb9f2691c1a27da2e3f3a4743
SHIMODA Hiroshi <shimoda@clear-code.com>
2009-09-01 02:51:55 +0000

    設定の書式を決定

---
+++ components/Startup.js
@@ -0,0 +1,60 @@
+const DEBUG = false;
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+Components.utils.import('resource://gre/modules/XPCOMUtils.jsm');
+
+const kCID  = Components.ID('{4048ba30-96a2-11de-8a39-0800200c9a66}'); 
+const kID   = '@clear-code.com/autopermission/startup;1';
+const kNAME = "Permissions Auto Registerer Startup Service";
+
+const ObserverService = Cc['@mozilla.org/observer-service;1']
+		.getService(Ci.nsIObserverService);
+
+const Pref = Cc['@mozilla.org/preferences;1']
+		.getService(Ci.nsIPrefBranch)
+
+function mydump()
+{
+	if (!DEBUG) return;
+	var str = Array.slice(arguments).join('\n');
+	if (str.charAt(str.length-1) != '\n') str += '\n';
+	dump(str);
+}
+ 
+function AutoPermissionStartupService() { 
+}
+AutoPermissionStartupService.prototype = {
+	 
+	observe : function(aSubject, aTopic, aData) 
+	{
+		switch (aTopic)
+		{
+			case 'app-startup':
+				ObserverService.addObserver(this, 'profile-do-change', false);
+				return;
+
+			case 'profile-do-change':
+				ObserverService.removeObserver(this, 'profile-do-change');
+				this.init();
+				return;
+		}
+	},
+
+	init : function() 
+	{
+	},
+
+  
+	QueryInterface : XPCOMUtils.generateQI([Ci.nsIObserver]),
+	_xpcom_categories : [
+		{ category : 'app-startup', service : true }
+	]
+ 
+}; 
+
+function NSGetModule(aCompMgr, aFileSpec)
+{
+	return XPCOMUtils.generateModule([AutoPermissionStartupService]);
+}
+
---
+++ defaults/preferences/autopermission.js
@@ -0,0 +1,13 @@
+/*
+	cookie      : cookies
+	geo         : HTML5 geolocation API
+	image       : images
+	install     : installation of XPI (addons)
+	offline-app : HTML5 offline API (storage, etc.)
+	popup       : popup blocker
+
+	0 = unknown
+	1 = allow
+	2 = deny
+*/
+// pref("extensions.autopermission.sites.www.example.com", "popup=1, geo=2, install=2");

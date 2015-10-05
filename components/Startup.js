c9b8d9a5f591d5f4c120b10bb16cafd04cf3e3de
YUKI Hiroshi <shimoda@clear-code.com>
2015-10-05 14:45:24 +0900

    Accept port number for a part of origin

--- components/Startup.js
+++ components/Startup.js
@@ -174,7 +174,7 @@ AutoPermissionStartupService.prototype = {
 	parsePermission : function(aValue) {
 		let origin;
 		if (aValue.indexOf(':') > 0) {
-			aValue = aValue.replace(/^\s*((?:https?\/\/)?[^:\s]+)\s*:\s*/, '');
+			aValue = aValue.replace(/^\s*((?:https?\/\/)?[^:\s]+(?::\d+)?)\s*:\s*/, '');
 			origin = RegExp.$1;
 		}
 		return {permission: aValue, origin: origin};
